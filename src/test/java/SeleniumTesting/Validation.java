2576de507431e548f92e023510616982c71b8607
sivaram511 <swamy@LAPTOP-49ALIVKJ>
2021-10-18 14:59:53 +0530

    t

--- src/test/java/SeleniumTesting/Validation.java
+++ src/test/java/SeleniumTesting/Validation.java
@@ -57,7 +57,7 @@ public class Validation extends Base {
 		Assert.assertFalse(true);
 	
 	}
-	//testingasd
+	//testingas
 	@Test(groups= {"abc"})
 	public void sell() throws InterruptedException {
 		rlogin=new RedBusLogin(driver);
